be4ec0220784656af638698a379b2978c4f365e6
Boundivore <boundivore@foxmail.com>
2023-09-06 19:35:55 +0800

    common fix

---
+++ .documents/plugins/KUBESPHERE/dlc/README.md
@@ -0,0 +1,8 @@
+此处存放各个服务的安装程序，格式为：
+
+${SERVICE_NAME}-${VERSION}.tar.gz
+为保持统一，该压缩包中解压出来的文件夹名称也必须为 ${SERVICE_NAME} (不带版本号，打包时请注意)
+
+举例：
+ZOOKEEPER-3.6.5.tar.gz
+该压缩包中解压出来的文件夹名称也必须为 ZOOKEEPER
---
+++ .documents/plugins/KUBESPHERE/templated/bin/create_project_harbor.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+# Copyright 2018 The KubeSphere Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+url="https://dockerhub.kubekey.local"
+user="admin"
+passwd="Harbor12345"
+
+harbor_projects=(library
+    kubesphereio
+    kubesphere
+    calico
+    coredns
+    openebs
+    csiplugin
+    minio
+    mirrorgooglecontainers
+    osixia
+    prom
+    thanosio
+    jimmidyson
+    grafana
+    elastic
+    istio
+    jaegertracing
+    jenkins
+    weaveworks
+    openpitrix
+    joosthofman
+    nginxdemos
+    fluent
+    kubeedge
+)
+
+for project in "${harbor_projects[@]}"; do
+    echo "creating $project"
+    curl -u "${user}:${passwd}" -X POST -H "Content-Type: application/json" "${url}/api/v2.0/projects" -d "{ \"project_name\": \"${project}\", \"public\": true}" -k
+done
