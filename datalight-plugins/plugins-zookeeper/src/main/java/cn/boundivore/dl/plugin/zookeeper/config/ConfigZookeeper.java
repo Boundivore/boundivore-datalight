163d20916f6c9feb1839a7cde6a3ce2cbaa8e40c
Boundivore <boundivore@foxmail.com>
2023-09-11 17:09:45 +0800

    common fix

--- datalight-plugins/plugins-zookeeper/src/main/java/cn/boundivore/dl/plugin/zookeeper/config/ConfigZookeeper.java
+++ datalight-plugins/plugins-zookeeper/src/main/java/cn/boundivore/dl/plugin/zookeeper/config/ConfigZookeeper.java
@@ -36,67 +36,6 @@ import java.util.LinkedHashMap;
 @Slf4j
 public class ConfigZookeeper extends AbstractConfig {
 
-    @Override
-    public PluginConfigResult configSelf() {
-        log.info("ConfigZookeeper 初始化自身配置");
-
-        PluginConfigResult pluginConfigResult = new PluginConfigResult(
-                super.currentMetaService.getPluginClusterMeta().getClusterId(),
-                super.currentMetaService.getServiceName(),
-                new LinkedHashMap<>()
-        );
-
-        super.currentMetaService.getConfDirList().forEach(i -> {
-
-            // 服务配置文件路径
-            final String serviceConfDirStr = super.trimDir(
-                    i.getServiceConfDir()
-            );
-
-            // 模板配置文件路径
-            final String templatedDirStr = super.trimDir(
-                    i.getTemplatedDir()
-            );
-
-            super.templatedFileList(templatedDirStr)
-                    .forEach(templatedFile -> {
-
-                        // 结合模板，补充用户提前配置信息，并返回修改后的模板
-                        String replacedTemplate = super.preConfig(templatedFile);
-
-                        // 得到最终配置文件数据(未 Base64)
-                        String configData = this.configLogic(
-                                templatedFile,
-                                replacedTemplate
-                        );
-
-                        // 组装 ConfigKey
-                        PluginConfigResult.ConfigKey configKey = super.assembleConfigKey(
-                                serviceConfDirStr,
-                                templatedFile,
-                                super.currentMetaComponent
-                        );
-
-                        // 组装 ConfigValue
-                        PluginConfigResult.ConfigValue configValue = super.assembleConfigValue(
-                                templatedFile,
-                                configData
-                        );
-
-                        // 存放到 Map 集合
-                        super.putConfig(
-                                pluginConfigResult,
-                                configKey,
-                                configValue
-                        );
-
-                    });
-
-        });
-
-        return pluginConfigResult;
-    }
-
     /**
      * Description: 根据配置文件执行不同的配置修改逻辑
      * Created by: Boundivore
