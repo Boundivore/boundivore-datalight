f7d215c2c881c51aa7403458fe57975d011e55af
Boundivore <boundivore@foxmail.com>
2023-12-27 16:47:31 +0800

    修复关闭 swap

--- .documents/assistant/scripts/init-close-swap.sh
+++ .documents/assistant/scripts/init-close-swap.sh
@@ -6,26 +6,45 @@ if [ "$EUID" -ne 0 ]; then
     exit 1
 fi
 
-# 禁用交换分区
+# 禁用所有激活的 swap 分区
 if ! swapoff -a; then
     echo "Disabling the swap partition failed."
     exit 1
 fi
 
-echo "Disabling the swap partition"
+echo "Swap partition(s) disabled."
 
-# 注释 /etc/fstab 中的交换分区配置
-sed -i 's/^\/dev\/mapper\/centos-swap swap/#&/' /etc/fstab
-
-# 设置 vm.swappiness 为 0
-grep -q '^vm.swappiness' /etc/sysctl.conf
-if [[ $? -eq 0 ]]; then
-    sed -i 's/^vm.swappiness.*/vm.swappiness=0/' /etc/sysctl.conf
-else
-    echo "vm.swappiness=0" >> /etc/sysctl.conf
+# 注释掉 /etc/fstab 中所有的 swap 相关行
+if ! sed -i '/\sswap\s/s/^/#/' /etc/fstab; then
+    echo "Failed to comment out swap entries in /etc/fstab."
+    exit 1
 fi
 
-sysctl vm.swappiness=0
+echo "Swap entries in /etc/fstab commented out."
 
-echo "$0 done."
+# 设置 vm.swappiness 为 0
+# 首先检查是否有 vm.swappiness 的设置存在
+if grep -q '^vm.swappiness' /etc/sysctl.conf; then
+    # 如果存在，则替换为新值
+    if ! sed -i 's/^vm.swappiness.*/vm.swappiness=0/' /etc/sysctl.conf; then
+        echo "Failed to set vm.swappiness to 0 in /etc/sysctl.conf."
+        exit 1
+    fi
+else
+    # 如果不存在，则添加设置
+    if ! echo "vm.swappiness=0" >> /etc/sysctl.conf; then
+        echo "Failed to add vm.swappiness setting to /etc/sysctl.conf."
+        exit 1
+    fi
+fi
+
+# 应用 sysctl 设置
+if ! sysctl -p; then
+    echo "Failed to reload sysctl settings."
+    exit 1
+fi
+
+echo "vm.swappiness set to 0 and sysctl settings reloaded."
+
+echo "$0 completed successfully."
 exit 0
--- .documents/node/scripts/check-swap-settings.sh
+++ .documents/node/scripts/check-swap-settings.sh
@@ -8,17 +8,24 @@ fi
 
 # 检查是否配置了正确的交换分区设置
 checkSwapSettings() {
-    # 检查是否存在交换分区配置
-    if grep -q "^#/dev/mapper/centos-swap" "/etc/fstab" && [[ "$(sysctl -n vm.swappiness)" -eq 0 ]]; then
+    # 检查/etc/fstab文件中是否有未被注释掉的swap分区
+    if ! grep -qE '^[^#].*\sswap\s' /etc/fstab; then
+        # 检查swappiness值是否为0
+        if [[ "$(sysctl -n vm.swappiness)" -eq 0 ]]; then
+            return 0
+        fi
+    fi
+    return 1
+}
+
+# 检查交换分区状态是否为关闭
+checkSwapStatus() {
+    # 检查系统上是否没有激活的swap分区
+    if [[ -z "$(swapon --show)" ]]; then
         return 0
     else
         return 1
     fi
-}
-
-# 检查交换分区是否已关闭
-checkSwapStatus() {
-    [[ -z "$(swapon --show)" ]]
 }
 
 # 检查交换分区配置是否正确
