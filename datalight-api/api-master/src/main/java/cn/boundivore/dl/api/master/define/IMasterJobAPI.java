20ef64ab8968a1f40dedbca640d0778eb32b8505
Boundivore <boundivore@foxmail.com>
2024-01-11 16:23:12 +0800

    完善查询服务、组件部署过程中的进度信息接口，进度70%

---
+++ datalight-api/api-master/src/main/java/cn/boundivore/dl/api/master/define/IMasterJobAPI.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright (C) <2023> <Boundivore> <boundivore@foxmail.com>
+ * <p>
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the Apache License, Version 2.0
+ * as published by the Apache Software Foundation.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Apache License, Version 2.0 for more details.
+ * <p>
+ * You should have received a copy of the Apache License, Version 2.0
+ * along with this program; if not, you can obtain a copy at
+ * http://www.apache.org/licenses/LICENSE-2.0.
+ */
+package cn.boundivore.dl.api.master.define;
+
+import cn.boundivore.dl.base.response.impl.master.AbstractJobVo;
+import cn.boundivore.dl.base.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import static cn.boundivore.dl.base.constants.IUrlPrefixConstants.MASTER_URL_PREFIX;
+
+
+/**
+ * Description: 作业异步任务管理相关
+ * Created by: Boundivore
+ * E-mail: boundivore@foxmail.com
+ * Creation time: 2024/1/11
+ * Modification description:
+ * Modified by:
+ * Modification time:
+ * Version: V1.0
+ */
+@Api(value = "IMasterJobAPI", tags = {"Master 接口：作业异步任务管理相关"})
+@FeignClient(
+        name = "IMasterJobAPI",
+        contextId = "IMasterJobAPI",
+        path = MASTER_URL_PREFIX
+)
+public interface IMasterJobAPI {
+
+    @GetMapping(value = "/job/progress")
+    @ApiOperation(notes = "获取作业任务进度", value = "获取作业任务进度")
+    Result<AbstractJobVo.JobProgressVo> getNodeJobProgress(
+            @ApiParam(name = "JobId", value = "JobId")
+            @RequestParam(value = "JobId", required = true)
+            Long jobId
+    ) throws Exception;
+
+}
---
+++ datalight-services/services-master/src/main/java/cn/boundivore/dl/service/master/controller/MasterJobController.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (C) <2023> <Boundivore> <boundivore@foxmail.com>
+ * <p>
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the Apache License, Version 2.0
+ * as published by the Apache Software Foundation.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Apache License, Version 2.0 for more details.
+ * <p>
+ * You should have received a copy of the Apache License, Version 2.0
+ * along with this program; if not, you can obtain a copy at
+ * http://www.apache.org/licenses/LICENSE-2.0.
+ */
+package cn.boundivore.dl.service.master.controller;
+
+import cn.boundivore.dl.api.master.define.IMasterJobAPI;
+import cn.boundivore.dl.base.response.impl.master.AbstractJobVo;
+import cn.boundivore.dl.base.result.Result;
+import cn.boundivore.dl.service.master.service.MasterJobService;
+import cn.dev33.satoken.annotation.SaCheckLogin;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Description: MasterJobController
+ * Created by: Boundivore
+ * E-mail: boundivore@foxmail.com
+ * Creation time: 2024/1/11
+ * Modification description:
+ * Modified by:
+ * Modification time:
+ * Version: V1.0
+ */
+@RestController
+@RequiredArgsConstructor
+@SaCheckLogin
+public class MasterJobController implements IMasterJobAPI {
+
+    private final MasterJobService masterJobService;
+
+
+    @Override
+    public Result<AbstractJobVo.JobProgressVo> getNodeJobProgress(Long jobId) throws Exception {
+        return this.masterJobService.getJobProgress(jobId);
+    }
+}
