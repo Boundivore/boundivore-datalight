264d64e81d0280579b8889c4b3ed8786c696f994
Boundivore <boundivore@foxmail.com>
2024-03-05 13:59:34 +0800

    增加用户锁

--- datalight-common/common-boot/src/main/java/cn/boundivore/dl/boot/lock/LocalLockAspect.java
+++ datalight-common/common-boot/src/main/java/cn/boundivore/dl/boot/lock/LocalLockAspect.java
@@ -1,7 +1,9 @@
 package cn.boundivore.dl.boot.lock;
 
+import cn.boundivore.dl.base.request.IRequest;
 import cn.boundivore.dl.exception.LockException;
 import cn.dev33.satoken.stp.StpUtil;
+import cn.hutool.core.exceptions.ExceptionUtil;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -13,6 +15,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
@@ -52,6 +55,19 @@ public class LocalLockAspect {
         String clusterId = StpUtil.getSession().get("clusterId", "0");
         String principal = StpUtil.getSession().get("principal", "defaultPrincipal");
 
+        Object[] args = joinPoint.getArgs();
+        for (Object arg : args) {
+            try {
+                String findClusterId = this.findClusterId(arg);
+                if (findClusterId != null) {
+                    clusterId = findClusterId;
+                    break;
+                }
+            } catch (IllegalAccessException e) {
+                log.error(ExceptionUtil.stacktraceToString(e));
+            }
+        }
+
         long timeout = localLockAnnotation.timeout();
         TimeUnit timeUnit = localLockAnnotation.unit();
 
@@ -88,6 +104,58 @@ public class LocalLockAspect {
      */
     private String generateKey(String clusterId, String principal) {
         return "LockKey::ClusterId:" + clusterId + "::Principal:" + principal;
+    }
+
+    /**
+     * Description: 查找参数中的 clusterId 属性
+     * Created by: Boundivore
+     * E-mail: boundivore@foxmail.com
+     * Creation time: 2024/3/5
+     * Modification description:
+     * Modified by:
+     * Modification time:
+     * Throws:
+     *
+     * @param obj 被反射函数的参数对象
+     * @return clusterId 查找 clusterId 属性
+     */
+    private String findClusterId(Object obj) throws IllegalAccessException {
+        if (obj == null) {
+            return null;
+        }
+
+        if(!(obj instanceof IRequest)){
+            return null;
+        }
+
+        // 获取对象类
+        Class<?> objClass = obj.getClass();
+        // 遍历所有字段
+        for (Field field : objClass.getDeclaredFields()) {
+            field.setAccessible(true);
+            // 如果是我们想找的字段
+            if ("clusterId".equals(field.getName())) {
+                // 获取该字段的值
+                return field.get(obj).toString();
+            } else if (IRequest.class.isAssignableFrom(field.getType())) {
+                // 如果该类型实现了 IRequest 接口，则继续查找
+                Object nestedObj = field.get(obj); // 获取字段的实例对象
+                if (nestedObj != null) {
+                    // 递归调用 findClusterId 方法
+                    String nestedClusterId = this.findClusterId(nestedObj);
+                    if (nestedClusterId != null) {
+                        // 如果在嵌套对象中找到了 clusterId 字段，则返回它的值
+                        return nestedClusterId;
+                    }
+                }
+            }
+        }
+
+        // 如果当前类没有找到，递归检查父类（如果需要）
+//        if (objClass.getSuperclass() != null) {
+//            return this.findClusterId(objClass.getSuperclass().newInstance());
+//        }
+        return null;
     }
 
 }
