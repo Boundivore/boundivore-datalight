99278063b6bbd112f14f7c60cf8367df1830d795
Boundivore <boundivore@foxmail.com>
2024-03-18 11:40:15 +0800

    修复部署 HIVE 时的错误

--- .documents/plugins/HIVE/templated/conf/hive-env.sh
+++ .documents/plugins/HIVE/templated/conf/hive-env.sh
@@ -62,6 +62,9 @@ export HIVE_PID_DIR={{HIVE_PID_DIR}}
 # Folder containing extra libraries required for hive compilation/execution can be controlled by:
 export HIVE_AUX_JARS_PATH={{HIVE_AUX_JARS_PATH}}
 
+export HADOOP_CLASSPATH="${HIVE_CONF_DIR}:${HIVE_AUX_JARS_PATH}/*"
+
+
 
 export HIVE_METASTORE_JMX_OPTS="-Djava.net.preferIPv4Stack=true \
 -Dcom.sun.management.jmxremote.authenticate=false \
