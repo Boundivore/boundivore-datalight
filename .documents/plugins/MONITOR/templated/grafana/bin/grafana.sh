6fcf75933fc1354ca4061f0903e3cb64ab56351f
Boundivore <boundivore@foxmail.com>
2024-04-15 15:41:17 +0800

    封装告警相关功能 20%

--- .documents/plugins/MONITOR/templated/grafana/bin/grafana.sh
+++ .documents/plugins/MONITOR/templated/grafana/bin/grafana.sh
@@ -3,13 +3,15 @@
 GRAFANA_HOME="{{SERVICE_DIR}}/MONITOR/grafana"
 GRAFANA_PATH="{{SERVICE_DIR}}/MONITOR/grafana/bin/grafana"
 GRAFANA_CONFIG="{{SERVICE_DIR}}/MONITOR/grafana/conf/defaults.ini"
+GRAFANA_LOG_PATH="{{LOG_DIR}}/MONITOR/grafana.log"
 
 echo "GRAFANA_HOME: ${GRAFANA_HOME}"
 echo "GRAFANA_PATH: ${GRAFANA_PATH}"
 echo "GRAFANA_CONFIG: ${GRAFANA_CONFIG}"
+echo "GRAFANA_LOG_PATH: ${GRAFANA_LOG_PATH}"
 
 start_grafana() {
-  nohup "${GRAFANA_PATH}" server --config "${GRAFANA_CONFIG}" --homepath "${GRAFANA_HOME}" > /dev/null 2>&1 &
+  nohup "${GRAFANA_PATH}" server --config "${GRAFANA_CONFIG}" --homepath "${GRAFANA_HOME}" > "${GRAFANA_LOG_PATH}" 2>&1 &
 }
 
 stop_grafana() {
