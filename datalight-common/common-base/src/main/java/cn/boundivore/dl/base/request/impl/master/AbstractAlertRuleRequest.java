28abaed2a624046d55907df647761c62aba8e72b
Boundivore <boundivore@foxmail.com>
2024-04-17 18:21:22 +0800

    封装告警相关功能 40%

--- datalight-common/common-base/src/main/java/cn/boundivore/dl/base/request/impl/master/AbstractAlertRuleRequest.java
+++ datalight-common/common-base/src/main/java/cn/boundivore/dl/base/request/impl/master/AbstractAlertRuleRequest.java
@@ -13,6 +13,7 @@ import lombok.experimental.Accessors;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Description: 告警相关请求体
@@ -42,9 +43,10 @@ public abstract class AbstractAlertRuleRequest {
         @JsonProperty(value = "AlertName", required = true)
         private String alertName;
 
-        @ApiModelProperty(name = "AlertRuleContentBase64", value = "告警配置内容(YAML 的 Base64 形式字符串)", required = true)
-        @JsonProperty(value = "AlertRuleContentBase64", required = true)
-        private String alertRuleContentBase64;
+        @ApiModelProperty(name = "AlertRuleContent", value = "告警配置内容实体", required = true)
+        @JsonProperty(value = "AlertRuleContent", required = true)
+        @NotNull(message = "规则实体不能为空")
+        private AlertRuleContentRequest alertRuleContent;
 
         @ApiModelProperty(name = "AlertHandlerTypeEnum", value = "告警触发时的处理类型", required = true)
         @JsonProperty(value = "AlertHandlerTypeEnum", required = true)
@@ -56,4 +58,75 @@ public abstract class AbstractAlertRuleRequest {
         private Long handlerId;
 
     }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Accessors(chain = true)
+    @Schema(
+            name = "AbstractAlertRuleRequest.AlertRuleContentRequest",
+            description = "AbstractAlertRuleRequest.AlertRuleContentRequest: 新建告警规则内容实体 请求体"
+    )
+    public final static class AlertRuleContentRequest implements IRequest {
+        private static final long serialVersionUID = -5838473919880577976L;
+
+        @ApiModelProperty(name = "Groups", value = "告警规则分组", required = true)
+        @JsonProperty(value = "Groups", required = true)
+        @NotEmpty(message = "告警规则分组不能为空")
+        private List<GroupRequest> groups;
+
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Accessors(chain = true)
+    @Schema(
+            name = "AbstractAlertRuleRequest.GroupRequest",
+            description = "AbstractAlertRuleRequest.GroupRequest: 告警规则实体中的分组 请求体"
+    )
+    public static class GroupRequest implements IRequest {
+        private static final long serialVersionUID = -4561495605113571769L;
+
+        @ApiModelProperty(name = "Name", value = "告警规则分组名称", required = true)
+        @JsonProperty(value = "Name")
+        private String name;
+
+        @ApiModelProperty(name = "Rules", value = "告警规则", required = true)
+        @JsonProperty(value = "Rules")
+        private List<RuleRequest> rules;
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Accessors(chain = true)
+    @Schema(
+            name = "AbstractAlertRuleRequest.AlertRuleContentRequest",
+            description = "AbstractAlertRuleRequest.AlertRuleContentRequest: 告警规则分组中的规则 请求体"
+    )
+    public static class RuleRequest implements IRequest {
+        private static final long serialVersionUID = -1194530752751114766L;
+
+        @ApiModelProperty(name = "Alert", value = "告警规则名称", required = true)
+        @JsonProperty(value = "Alert")
+        private String alert;
+
+        @ApiModelProperty(name = "Expr", value = "告警规则判断表达式", required = true)
+        @JsonProperty(value = "Expr")
+        private String expr;
+
+        @ApiModelProperty(name = "For", value = "满足 Expr 表达式多久后执行告警", required = true)
+        @JsonProperty(value = "For")
+        private String duration;
+
+        @ApiModelProperty(name = "Labels", value = "告警规则标签", required = true)
+        @JsonProperty(value = "Labels")
+        private Map<String, String> labels;
+
+        @ApiModelProperty(name = "Annotations", value = "告警规则注解", required = true)
+        @JsonProperty(value = "Annotations")
+        private Map<String, String> annotations;
+    }
+
 }
