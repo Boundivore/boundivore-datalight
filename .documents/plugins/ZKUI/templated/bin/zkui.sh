57194edef5eb388f6a35d4b3596323c85c091cb3
Boundivore <boundivore@foxmail.com>
2024-05-13 12:54:49 +0800

    修复部分 BUG，增加节点序列功能

--- .documents/plugins/ZKUI/templated/bin/zkui.sh
+++ .documents/plugins/ZKUI/templated/bin/zkui.sh
@@ -17,9 +17,11 @@ LOG_FILE="${LOG_DIR}/${SERVICE_NAME}/zkui.out"
 # 获取当前脚本所在目录
 bin_dir=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
 
-jar_path="${bin_dir}/zkui-2.0.0.jar"
+jar_name="zkui-2.0.0.jar"
+jar_path="${bin_dir}/${jar_name}"
 log_config_path="file:${bin_dir}/log4j.properties"
 
+# shellcheck disable=SC2164
 cd "${bin_dir}"
 
 # 确保 PID 和日志目录存在
@@ -48,18 +50,15 @@ start() {
 }
 
 stop() {
-    echo "Stopping ${SERVICE_NAME} ..."
-    if [ ! -f "${PID_FILE}" ]; then
-        echo "${SERVICE_NAME} is not running."
-        exit 1
-    fi
-    PID=$(cat "${PID_FILE}")
-    if kill "${PID}" > /dev/null 2>&1; then
-        echo "${SERVICE_NAME} stopped."
-        rm "${PID_FILE}"
-    else
-        echo "Failed to stop ${SERVICE_NAME}."
-    fi
+  echo "Stopping ${SERVICE_NAME} ..."
+      local pids
+      pids=$(ps -aux | grep "[j]ava .*${jar_name}" | awk '{print $2}')
+      if [ -n "${pids}" ]; then
+          kill -9 ${pids}
+          echo "${SERVICE_NAME} stopped successfully."
+      else
+          echo "No ${SERVICE_NAME} process found."
+      fi
 }
 
 case "$1" in
