a650e7e35e692b81ebc66855e38bb4245db419b0
Boundivore <boundivore@foxmail.com>
2024-05-15 12:12:41 +0800

    init

--- .documents/exporter/bin/example.sh
+++ .documents/exporter/bin/example.sh
@@ -1,2 +1,9 @@
 # java -javaagent:./jmx_exporter.jar=12345:config.yaml -jar yourJar.jar
+#export YARN_RESOURCEMANAGER_OPTS="-Djava.net.preferIPv4Stack=true \
+#-Dcom.sun.management.jmxremote.authenticate=false \
+#-Dcom.sun.management.jmxremote.ssl=false \
+#-Dcom.sun.management.jmxremote.local.only=false \
+#-Dcom.sun.management.jmxremote.port={{jmxRemotePort_ResourceManager}} \
+#-javaagent:${DATALIGHT_DIR}/exporter/jar/jmx_exporter.jar={{jmxExporterPort_ResourceManager}}:${SERVICE_DIR}/YARN/exporter/conf/jmx_config_ResourceManager.yaml"
+
 SERVER_JVMFLAGS="-javaagent:{{DATALIGHT_DIR}}/exporter/jar/jmx_exporter.jar={{exporterPort}}:{{JMX_CONF_FILE}}"
