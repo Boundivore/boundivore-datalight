819d9640eee8acce6ef602aef8cffccb7eb80057
Boundivore <boundivore@foxmail.com>
2024-05-22 11:43:34 +0800

    init

--- datalight-services/services-master/src/test/java/cn/boundivore/dl/service/master/test/CoreJavaTest.java
+++ datalight-services/services-master/src/test/java/cn/boundivore/dl/service/master/test/CoreJavaTest.java
@@ -154,7 +154,7 @@ public class CoreJavaTest {
         ChatResponse response = qianfan.chatCompletion()
                 .model("ERNIE-Speed-128K") // 使用model指定预置模型
                 // .endpoint("completions_pro") // 也可以使用endpoint指定任意模型 (二选一)
-                .addMessage("user", "如何学习 Java 编程") // 添加用户消息 (此方法可以调用多次，以实现多轮对话的消息传递)
+                .addMessage("user", "你好") // 添加用户消息 (此方法可以调用多次，以实现多轮对话的消息传递)
                 .temperature(0.7) // 自定义超参数
                 .execute(); // 发起请求
         System.out.println(response.getResult());
