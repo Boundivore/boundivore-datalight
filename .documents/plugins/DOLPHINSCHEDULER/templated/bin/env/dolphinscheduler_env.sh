39eaf1d150982867dbdacb46692698622a637522
Boundivore <boundivore@foxmail.com>
2024-10-26 23:37:39 +0800

    NEW:1、新增 DolphinScheduler 部署逻辑

--- .documents/plugins/DOLPHINSCHEDULER/templated/bin/env/dolphinscheduler_env.sh
+++ .documents/plugins/DOLPHINSCHEDULER/templated/bin/env/dolphinscheduler_env.sh
@@ -27,7 +27,7 @@ export SPRING_DATASOURCE_PASSWORD="{{db_root_password}}"
 
 # DolphinScheduler server related configuration
 export SPRING_CACHE_TYPE=${SPRING_CACHE_TYPE:-none}
-export SPRING_JACKSON_TIME_ZONE=${SPRING_JACKSON_TIME_ZONE:-UTC}
+export SPRING_JACKSON_TIME_ZONE="GMT+8"
 export MASTER_FETCH_COMMAND_NUM=${MASTER_FETCH_COMMAND_NUM:-10}
 
 # Registry center configuration, determines the type and link of the registry center
